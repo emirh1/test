27fc4acfd6487458511104542c5f25eec989261f
Emir Hrbat <hrb.emir@gmail.com>
2023-02-05 00:34:23 +0100

    drugi commit

--- foo.cpp
+++ foo.cpp
@@ -1,6 +1,7 @@
-#include<iostream>
-int main()
-{
-std::cout<<"test";
-return 0 ;
+#include <iostream>
+int main() {
+  std::cout << "test";
+  std::cout << std::endl;
+  std::cout << "promjena";
+  return 0;
 }
